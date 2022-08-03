721c8717f770a3bee9f5b9e78f3edd9c3f24bdf4
GiordiR <riki.giordi@hotmail.it>
2022-08-03 15:50:32 +0200

    Added double integral method and test

--- src/integral/integral.py
+++ src/integral/integral.py
@@ -34,7 +34,7 @@ def midpoint(f, a, b, n):
     .. math ::
         \int_{a}^{b} f(x) dx \approx h \sum_{i=0}^{n-1} f(x_i)
 
-        where, x_i = (a + \frac{h}{2} + ih
+        where, x_i = a + \frac{h}{2} + ih
 
     :param f: function.
     :type f: lambda function
@@ -56,6 +56,83 @@ def midpoint(f, a, b, n):
     result *= h
 
     return result
+
+
+def midpoint_double(f, a, b, c, d, nx, ny):
+    r"""
+    Composite trapezoidal method for double integral numerical calculation.
+
+    .. math ::
+        \int_{a}^{b} \int_{c}^{d} f(x, y) dydx \approx h_x h_y \sum_{i=0}^{n_x-1} \sum_{j=0}^{n_y-1} f(x_i, y_j)
+
+        where, x_i = a + \frac{h_x}{2} + ih_x \\
+               y_j = c + \frac{h_y}{2} + jh_y
+
+    :param f: function.
+    :type f: lambda function
+
+    :param a: Lower interval bound in x.
+    :type a: float
+
+    :param b: Upper interval bound in x.
+    :type b: float
+
+    :param c: Lower interval bound in y.
+    :type c: float
+
+    :param d: Upper interval bound in y.
+    :type d: float
+
+    :param nx: Number of subdivision in x.
+    :type nx: int
+
+    :param ny: Number of subdivision in y.
+    :type ny: int
+    """
+
+    hx = (b - a)/float(nx)
+    hy = (d - c)/float(ny)
+    Integral = 0
+    for i in range(nx):
+        for j in range(ny):
+            xi = a + hx/2 + i*hx
+            yj = c + hy/2 + j*hy
+            Integral += hx * hy * f(xi, yj)
+
+    return Integral
+
+
+def midpoint_double2(f, a, b, c, d, nx, ny):
+    r"""
+    Composite trapezoidal method for double integral numerical calculation.
+    Reusing 1D formulation
+
+    :param f: function.
+    :type f: lambda function
+
+    :param a: Lower interval bound in x.
+    :type a: float
+
+    :param b: Upper interval bound in x.
+    :type b: float
+
+    :param c: Lower interval bound in y.
+    :type c: float
+
+    :param d: Upper interval bound in y.
+    :type d: float
+
+    :param nx: Number of subdivision in x.
+    :type nx: int
+
+    :param ny: Number of subdivision in y.
+    :type ny: int
+    """
+
+    def g(x):
+        return midpoint(lambda y: f(x, y), c, d, ny)
+
+    return midpoint(g, a, b, nx)
 
 
 # TODO: Simpson's rule, Gauss quadrature
--- tests/test_integrals.py
+++ tests/test_integrals.py
@@ -174,3 +174,23 @@ def test_midpoint_vec_linear():
         success = error < tol
         msg = 'error=%g > tol0%g' % (error, tol)
         assert (success, msg)
+
+
+def test_midpoint_double():
+    """Test that a linear function is integrated exactly."""
+
+    def f(x, y):
+        return 2*x + y
+
+    a = 0; b = 2; c = 2; d = 3
+    import sympy
+    x, y = sympy.symbols('x y')
+    I_expected = sympy.integrate(f(x, y), (x, a, b), (y, c, d))
+    # Test three cases: nx < ny, nx = ny, nx > ny
+    for nx, ny in (3, 5), (4, 4), (5, 3):
+        I_computed1 = midpoint_double(f, a, b, c, d, nx, ny)
+        I_computed2 = midpoint_double2(f, a, b, c, d, nx, ny)
+        tol = 1E-14
+        assert(abs(I_computed1 - I_expected) < tol)
+        assert(abs(I_computed2 - I_expected) < tol)
+
