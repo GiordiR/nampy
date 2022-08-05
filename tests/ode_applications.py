a3ebd6d85355343581681c32d9c5a3fdbc7decac
GiordiR <riki.giordi@hotmail.it>
2022-08-05 16:44:51 +0200

    Added ode methods, applications and tests

--- tests/ode_applications.py
+++ tests/ode_applications.py
@@ -1,14 +1,148 @@
 import matplotlib.pyplot as plt
 from src.ode.ode import *
+from numpy import exp, pi, cos
+
 
 def application_ode_FE():
     """ Population growth demo"""
+
     def f(u, t):
-        return 0.1*u
+        return 0.1 * u
 
     u, t = ode_FE(f, 100, 0.5, 20)
     plt.plot(t, u, label='Numerical')
-    plt.plot(t, 100*exp(0.1*t), label='Exact')
+    plt.plot(t, 100 * exp(0.1 * t), label='Exact')
     plt.legend()
     plt.grid()
+    plt.show()
+
+
+def application_ode_system_FE():
+    """ Spreading diseases demo """
+
+    def f(u, t):
+        S, I, R = u
+        return [-beta * S * I, beta * S * I - gamma * I, gamma * I]
+
+    beta = 10. / (40 * 8 * 24)
+    gamma = 3. / (15 * 24)
+    dt = 0.1
+    D = 30
+    Nt = int(D * 24 / dt)
+    T = dt * Nt
+    U0 = [50, 1, 0]
+
+    u, t = ode_system_FE(f, U0, dt, T)
+
+    S = u[:, 0]
+    I = u[:, 1]
+    R = u[:, 2]
+
+    # Solution plot
+    fig = plt.figure()
+    l1, l2, l3 = plt.plot(t, S, t, I, t, R)
+    fig.legend((l1, l2, l3), ('S', 'I', 'R'), 'lower right')
+    plt.xlabel('hours')
+    plt.show()
+
+    # Consistency check
+    N = S[0] + I[0] + R[0]
+    eps = 1E-12
+    for n in range(len(S)):
+        SIR_sum = S[n] + I[n] + R[n]
+        if abs(SIR_sum - N) > eps:
+            print('*** consistency check failed: S+I+R=%g != %g' % (SIR_sum, N))
+
+
+def application_ode_EC_linear_damping():
+    """ Linear damping demo"""
+    b = 0.3
+    f = lambda v: b * v
+    s = lambda u: k * u
+    F = lambda t: 0
+
+    m = 1
+    k = 1
+    U_0 = 1
+    V_0 = 0
+
+    T = 12 * pi
+    dt = T / 5000.
+
+    u, v, t = ode_EulerCromer(f=f, s=s, F=F, m=m, T=T,
+                              U0=U_0, V0=V_0, dt=dt)
+    plot_u(u, t)
+
+
+def application_ode_EC_linear_damping_sine_excitation():
+    """ Linear damping with sine excitation demo"""
+    b = 0.3
+    f = lambda v: b * v
+    s = lambda u: k * u
+    from math import pi, sin
+    w = 1
+    A = 0.5
+    F = lambda t: A * sin(w * t)
+
+    m = 1
+    k = 1
+    U_0 = 1
+    V_0 = 0
+
+    T = 12 * pi
+    dt = T / 5000.
+
+    u, v, t = ode_EulerCromer(f=f, s=s, F=F, m=m, T=T,
+                              U0=U_0, V0=V_0, dt=dt)
+    plot_u(u, t)
+
+
+def application_ode_EC_sliding_friction():
+    """ Sliding friction demo"""
+    from numpy import tanh, sign
+
+    f = lambda v: mu * m * g * sign(v)
+    alpha = 60.0
+    s = lambda u: k / alpha * tanh(alpha * u)
+    s = lambda u: k * u
+    F = lambda t: 0
+
+    g = 9.81
+    mu = 0.4
+    m = 1
+    k = 1000
+
+    U_0 = 0.1
+    V_0 = 0
+
+    T = 2
+    dt = T / 5000.
+
+    u, v, t = ode_EulerCromer(f=f, s=s, F=F, m=m, T=T,
+                              U0=U_0, V0=V_0, dt=dt)
+    plot_u(u, t)
+
+
+def plot_u(u, t, percentage=100, heading='', labels=('t', 'u')):
+    index = int(len(u) * percentage / 100)
+    plt.plot(t[-index:], u[-index:], 'b-')
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    plt.title(heading)
+    plt.show()
+
+
+def application_ode_RK2():
+    """ Oscillating system RK2 demo"""
+    omega = 2
+    P = 2 * pi / omega
+    dt = P / 20
+    T = 10 * P
+    X0 = 2
+    u, v, t = ode_RK2(X0, omega, dt, T)
+
+    fig = plt.figure()
+    l1, l2 = plt.plot(t, u, 'b-', t, X0 * cos(omega * t), 'r--')
+    fig.legend((l1, l2), ('numerical', 'exact'), 'upper left')
+    plt.xlabel('t')
     plt.show()
--- tests/test_ode.py
+++ tests/test_ode.py
@@ -5,10 +5,10 @@ def test_ode_FE():
     """ Test that a linear u(t)=a*t+b is exactly reproduced """
 
     def exact_solution(t):
-        return a*t + b
+        return a * t + b
 
     def f(u, t):
-        return a + (u - exact_solution(t))**m
+        return a + (u - exact_solution(t)) ** m
 
     a = 4
     b = -1
@@ -22,3 +22,99 @@ def test_ode_FE():
     success = diff < tol
 
     assert success
+
+
+def test_ode_EC_undamped_linear():
+    """Test Euler-Cromer method for a linear problem."""
+    from numpy import pi
+    omega = 2
+    P = 2 * pi / omega
+    dt = P / 20
+    T = 40 * P
+    exact_v = -3.5035725322034139
+    exact_u = 0.7283057044967003
+    computed_u, computed_v, t = ode_EulerCromer(
+        f=lambda v: 0, s=lambda u: omega ** 2 * u,
+        F=lambda t: 0, m=1, T=T, U0=2, V0=0, dt=dt)
+    diff_u = abs(exact_u - computed_u[-1])
+    diff_v = abs(exact_v - computed_v[-1])
+    tol = 1E-14
+    assert diff_u < tol and diff_v < tol
+
+
+def _test_manufactured_solution(damping=True):
+    import sympy as sp
+    t, m, k, b = sp.symbols('t m k b')
+    # Choose solution
+    u = sp.sin(t)
+    v = sp.diff(u, t)
+    # Choose f, s, F
+    f = b * v
+    s = k * sp.tanh(u)
+    F = sp.cos(2 * t)
+
+    equation = m * sp.diff(v, t) + f + s - F
+
+    # Adjust F (source term because of manufactured solution)
+    F += equation
+    print('F:', F)
+
+    # Set values for the symbols m, b, k
+    m = 0.5
+    k = 1.5
+    b = 0.5 if damping else 0
+    F = F.subs('m', m).subs('b', b).subs('k', k)
+
+    print(f, s, F)
+    # Turn sympy expression into Python function
+    F = sp.lambdify([t], F)
+    # Define Python functions for f and s
+    # (the expressions above are functions of t, we need
+    # s(u) and f(v)
+    from numpy import tanh
+    s = lambda u: k * tanh(u)
+    f = lambda v: b * v
+
+    # Add modules='numpy' such that exact u and v work
+    # with t as array argument
+    exact_u = sp.lambdify([t], u, modules='numpy')
+    exact_v = sp.lambdify([t], v, modules='numpy')
+
+    # Solve problem for different dt
+    from numpy import pi, sqrt, sum, log
+    P = 2 * pi
+    time_intervals_per_period = [20, 40, 80, 160, 240]
+    h = []  # store discretization parameters
+    E_u = []  # store errors in u
+    E_v = []  # store errors in v
+
+    for n in time_intervals_per_period:
+        dt = P / n
+        T = 8 * P
+        computed_u, computed_v, t = ode_EulerCromer(
+            f=f, s=s, F=F, m=m, T=T,
+            U0=exact_u(0), V0=exact_v(0), dt=dt)
+
+        error_u = sqrt(dt * sum((exact_u(t) - computed_u) ** 2))
+        error_v = sqrt(dt * sum((exact_v(t) - computed_v) ** 2))
+        h.append(dt)
+        E_u.append(error_u)
+        E_v.append(error_v)
+
+    # Compute convergence rates
+    r_u = [log(E_u[i] / E_u[i - 1]) / log(h[i] / h[i - 1])
+           for i in range(1, len(h))]
+    r_v = [log(E_u[i] / E_u[i - 1]) / log(h[i] / h[i - 1])
+           for i in range(1, len(h))]
+    tol = 0.02
+    exact_r_u = 1.0 if damping else 2.0
+    exact_r_v = 1.0 if damping else 2.0
+    success = abs(exact_r_u - r_u[-1]) < tol and \
+              abs(exact_r_v - r_v[-1]) < tol
+    msg = ' u rate: %.2f, v rate: %.2f' % (r_u[-1], r_v[-1])
+    assert success, msg
+
+
+def test_manufactured_solution_ode_EC():
+    _test_manufactured_solution(damping=True)
+    _test_manufactured_solution(damping=False)
